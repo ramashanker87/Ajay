7e0289716863e4725b1c2799854ac2d13de7e8c8
admin <ajayjatala01@gmail.com>
2025-01-23 21:39:17 -0800

    Day 01 Assignment

---
+++ Day01/first-java-project/src/main/java/com/rama/app/App.java
@@ -0,0 +1,13 @@
+package com.rama.app;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
