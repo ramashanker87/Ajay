0ae1e7b5c1011f889892f50436824838676e2cb0
admin <ajayjatala01@gmail.com>
2025-01-26 20:48:19 -0800

    Day 05 Lab

---
+++ Day05/calculator/src/test/java/com/ajay/app/CalTest.java
@@ -0,0 +1,23 @@
+package com.ajay.app;
+
+import org.junit.Test;
+public class CalTest {
+    @Test
+    public void testAdd() {
+        Cal calculator = new Cal();
+        double result=calculator.add(11,42);
+        assert result==53;
+    }
+    @Test
+    public void testSubtract() {
+        Cal calculator = new Cal();
+        double result = calculator.subtract(65,15);
+        assert result==50;
+    }
+    @Test
+    public void testMultiply() {
+        Cal calculator = new Cal();
+        double result= calculator.multiply(5,10);
+        assert result==50;
+    }
+}
