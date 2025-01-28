354f48d91cdc95a580e806e94d580f75d0209f25
admin <ajayjatala01@gmail.com>
2025-01-27 21:10:03 -0800

    Day06 lab

---
+++ Day06/springboot-app/src/main/java/com/ajay/app/App.java
@@ -0,0 +1,17 @@
+package com.ajay.app;
+
+/**
+ * Hello world!
+ *
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigures.SpringApplication;
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+       SpringApplication.run(App.class, args );
+    }
+}
