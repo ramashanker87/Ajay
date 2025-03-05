202ef0b5a0bfd5ccdf61a39a7e6ebfd0e7ab6362
admin <rama@gail.com>
2025-03-05 04:42:28 -0800

    Day 13

---
+++ Day13/patient-management-app/src/test/java/com/ajay/app/PatientManagementAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ajay.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class PatientManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
+
